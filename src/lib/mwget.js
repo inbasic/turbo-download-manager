44d42e2781c2de50b931a9a63c3e28d01ffa273c
InBasic <inb.cor@gmail.com>
2016-01-04 11:38:33 +0330

    deleting unfinished files; firefox

--- src/lib/mwget.js
+++ src/lib/mwget.js
@@ -174,9 +174,7 @@ if (typeof require !== 'undefined') {
       if (wget.status === 'download') {
         throw Error('Cannot remove an instance while it is active. Try to pause the download first');
       }
-      console.error(wget.status);
-      if (wget.status === 'pause') {
-        console.error('hereerere');
+      if (wget.status === 'pause' || wget.status === 'error') {
         wget['internals@b'].file.remove();
       }
       delete instances[index];
